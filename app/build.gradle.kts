d9ed4e5b69c3b41f95c10644542bb023a662182c
FAISEL95595 <86416339+FAISEL95595@users.noreply.github.com>
2026-04-13 22:40:00 +0300

    Update versionName to '1.2' in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 28
         targetSdk = 36
         versionCode = 1
-        versionName = appVersionName
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 

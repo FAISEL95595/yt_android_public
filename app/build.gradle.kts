d7ee3ca7f18adb56c6d163a915ee0ee4e564e4d2
Faisel95595 <elhananmoshe95595@gmail.com>
2026-04-14 13:53:06 +0300

    vers code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.faisel.ytdlf"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.2"
+        versionCode = 2
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
